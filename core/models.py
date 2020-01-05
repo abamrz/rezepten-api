205d81cd8d0efda4d7322b64f4feff6ba1af22e4
abamrz <mirzakhidov@mail.ru>
2020-01-05 20:52:54 +0100

    feature: implement user, superuser creating funcs through models

--- core/models.py
+++ core/models.py
@@ -6,10 +6,20 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create new user, then save him"""
+        if not email:
+            raise ValueError('User must provide email address')
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        """Create superuser, then save him"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
         return user
 
 
