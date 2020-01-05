0ddc4c524a2118dec569f1af7918bdc69817ae74
abamrz <mirzakhidov@mail.ru>
2020-01-05 20:53:55 +0100

    test: check email normalization, invalid email, superuser creation

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -15,3 +15,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email of new user is normalized"""
+        email = 'example@gmail.de'
+        user = get_user_model().objects.create_user(email, 'password')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_email_invalid(self):
+        """Test invalid email value error of user"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'PASSWORD')
+
+    def test_create_superuser(self):
+        """Test for creating superuser"""
+        user = get_user_model().objects.create_user(
+            'example@gmail.com',
+            'password'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertEqual(user.is_staff)
